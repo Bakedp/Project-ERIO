e3513463b6fd23c687b1bf6170ba81501ef7e6db
Bakedp <danielsimonelli23@gmail.com>
2020-04-06 15:26:38 +0200

    a

--- Script/KinematicBody2D.gd
+++ Script/KinematicBody2D.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
-const SPEED = 5 * 96 #96 è la tile size
-const GRAVITY = 1500
-const JUMP_POWER = -400
+const SPEED = 1000
+const GRAVITY = 3000
+const JUMP_POWER = -2000
 const FLOOR = Vector2(0, -1)
 
 var velocity = Vector2()
@@ -13,15 +13,22 @@ func _physics_process(delta):
 
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = SPEED
+		$AnimatedSprite.play("run")
+		$AnimatedSprite.flip_h = false
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -SPEED
+		$AnimatedSprite.play("run")
+		$AnimatedSprite.flip_h = true
 	else:
 		velocity.x = 0
+		if on_ground == true:
+			$AnimatedSprite.play("idle")
 
 	if Input.is_action_just_pressed("ui_up") && jump_count < 2:
 		jump_count += 1
 		velocity.y = JUMP_POWER
 		on_ground = false
+		
 
 	velocity.y += delta * GRAVITY
 	velocity = move_and_slide(velocity, FLOOR)
@@ -34,3 +41,7 @@ func _physics_process(delta):
 		if on_ground == true:
 			on_ground = false
 			jump_count = 1
+		if velocity.y < 0:
+			$AnimatedSprite.play("jump")
+		else:
+			$AnimatedSprite.play("fall")
